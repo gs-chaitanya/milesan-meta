03f5a95c416cd2fbd2017866646df1d1ab67f2e8
Tobias Kovats <tkovats@student.ethz.ch>
2024-02-15 09:41:09 +0000

    remove tmp files

--- fuzzer/params/runparams.py
+++ fuzzer/params/runparams.py
@@ -16,6 +16,6 @@ PATH_TO_FIGURES = os.environ['CASCADE_PATH_TO_FIGURES']
 DO_ASSERT = True
 DO_EXPENSIVE_ASSERT = False # More expensive assertions
 
-NO_REMOVE_TMPFILES = True # Used for debugging purposes.
+NO_REMOVE_TMPFILES = False # Used for debugging purposes.
 
 RUN_TIMEOUT_SECONDS = 60*60*2 # A program is not supposed to run longer than this in RTL simulation.
