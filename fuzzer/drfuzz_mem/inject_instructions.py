df70633f0c04c57f83b48181159e3eaa36358b16
Tobias Kovats <tkovats@student.ethz.ch>
2024-02-21 17:17:43 +0000

    enables taintless fuzzing, adds en_taint flag and refactors

--- fuzzer/drfuzz_mem/inject_instructions.py
+++ fuzzer/drfuzz_mem/inject_instructions.py
@@ -6,47 +6,36 @@ import subprocess, itertools
 from common import designcfgs
 MAX_CYCLES_PER_INSTR = 30
 SETUP_CYCLES = 1000 # Without this, we had issues with BOOM with very short programs (typically <20 instructions) not being able to finish in time.
-
-def gen_elf_and_inject_taints(design_name, max_n_insts_per_bb, seed):
-    # fuzzerstate, rtl_elfpath, interm_elfpath, _, _, _, _  = gen_fuzzerstate_elf_expectedvals(*gen_new_test_instance(DESIGN_NAME, 0, True), True)
-    
+def gen_elf_and_inject_instructions(design_name, max_n_insts_per_bb, en_taint,seed):    
     fuzzerstate, interm_elfpath  = gen_fuzzerstate_elf_expectedvals_interm(*gen_new_test_instance(design_name, seed, True), True)
     ID = fuzzerstate.instance_to_str()
-    cov_dir = os.path.join(PATH_TO_COV,fuzzerstate.design_name,'drfuzz_mem',ID,"cov")
-    q_dir = os.path.join(PATH_TO_COV,fuzzerstate.design_name,'drfuzz_mem',ID,"queue")
-    mut_inst_path = os.path.join(PATH_TO_TMP, f'{ID}.mut_inst.json')
-    tracefile = os.path.join(PATH_TO_TMP, f'{ID}.trace.vcd')
-    env_path = os.path.join(PATH_TO_TMP, f'{ID}.env.sh')
-    exclude_regs = {0,1,2,3,4,5,8,9}
-    # exclude_reg_imm_insts = {"slliw","srliw","sraiw","slli","srli","srai"} # #cycles for shifts depends on immediate value thus taints pc
-    # exclude_insts |= {"addi","addiw"} # how do they influnce taint?
-    # mut_insts = {"slliw","srliw","sraiw","slli","srli","srai"} 
+    root_dir = os.path.join(PATH_TO_COV,fuzzerstate.design_name,"drfuzz" if en_taint else "rfuzz", ID)
+    cov_dir = os.path.join(root_dir,"cov")
+    q_dir = os.path.join(root_dir,"queues")
+    ## temp dirs below
+    mut_dir = os.path.join(PATH_TO_TMP, 'mut_insts')
+    mut_inst_path = os.path.join(mut_dir, f'{ID}.mut_inst.json')
+    trace_dir = os.path.join(PATH_TO_TMP, 'tracefiles')
+    tracefile = os.path.join(trace_dir, f'{ID}.trace.vcd')
+    env_dir = os.path.join(PATH_TO_TMP, 'envs')
+    env_path = os.path.join(env_dir,f'{ID}.env.sh')
+
     insts = {}
     for bb_id ,(bb_start_addr, bb_instrs) in enumerate(zip(fuzzerstate.bb_start_addr_seq[:-1], fuzzerstate.instr_objs_seq[:-1])): # skip first and last bb
         if bb_id == 0: continue
         insts[bb_id] = []
         for instr_id_in_bb, instr_obj in enumerate(bb_instrs):
-            if isinstance(instr_obj, RegImmInstruction) or isinstance(instr_obj, R12DInstruction):
-                # if instr_obj.instr_str not in mut_insts: continue
+            if isinstance(instr_obj, RegImmInstruction):
                 addr = bb_start_addr + 4*instr_id_in_bb
                 insts[bb_id] += [{"bytecode":instr_obj.gen_bytecode_int(is_spike_resolution=True),
                                 "bytecode_t0":instr_obj.gen_bytecode_int_t0(is_spike_resolution=True),
                                 "addr": addr,
-                                "type":"I", 
+                                "type":"I" if isinstance(instr_obj, RegImmInstruction) else "R", 
                                 "str": instr_obj.instr_str,
                                 "bb_id":bb_id}]
-            # elif isinstance(instr_obj, R12DInstruction):
-            #     if len({instr_obj.rs1, instr_obj.rs2, instr_obj.rd} & exclude_regs): continue # at 0x5 the temp regs start, below is tp, gp, sp, ra and zero which we dont want to mutate on
-            #     addr = bb_start_addr + 4*instr_id_in_bb
-            #     insts[bb_id] += [{"bytecode":instr_obj.gen_bytecode_int(is_spike_resolution=True),
-            #                     "bytecode_t0": instr_obj.gen_bytecode_int_t0(is_spike_resolution=True), 
-            #                     "addr": addr,
-            #                     "type":"R", 
-            #                     "str": instr_obj.instr_str,
-            #                     "bb_id":bb_id}] # dont taint rs2 if its tp, sp etc
 
     inst_str_blocks = []
-    assert(len(insts)), "No instructions chosen for injection."
+    assert(len(insts)), "No instructions chosen for injection, exiting."
     for bb_start_addr, block_insts in insts.items():
         if len(block_insts) == 0: continue
         chosen_idxs = [] # keep track of chosen instructions so we dont have duplicates within a block
@@ -74,7 +63,7 @@ def gen_elf_and_inject_taints(design_name, max_n_insts_per_bb, seed):
     
     inst_str += inst_str_blocks[-1] + "\n]" 
 
-
+    os.makedirs(mut_dir,exist_ok=True)
     with open(mut_inst_path, "w") as f:
         f.write(inst_str)
 
@@ -86,12 +75,14 @@ def gen_elf_and_inject_taints(design_name, max_n_insts_per_bb, seed):
     env["ID"] = ID
     env["SIMLEN"] =str(num_instrs*MAX_CYCLES_PER_INSTR + SETUP_CYCLES)
     env["MUT_INST_PATH"] = mut_inst_path
-    env["COV_DIR"] = cov_dir
     env["Q_DIR"] = q_dir
+    env["COV_DIR"] = cov_dir
     env["TRACEFILE"] = tracefile
-    cmd = ["make","rerun_drfuzz_mem_notrace"]
-    os.makedirs(q_dir)
-    os.makedirs(cov_dir)
+    cmd = ["make","rerun_drfuzz_mem_notrace" if en_taint else "rerun_rfuzz_mem_notrace"]
+    os.makedirs(cov_dir,exist_ok=True)
+    os.makedirs(q_dir,exist_ok=True)
+    os.makedirs(trace_dir,exist_ok=True)
+    os.makedirs(env_dir,exist_ok=True)
 
     print(f"source {env_path}")
     with open(env_path, "w") as f:
@@ -100,14 +91,11 @@ def gen_elf_and_inject_taints(design_name, max_n_insts_per_bb, seed):
         f.write(f"export MUT_INST_PATH={env['MUT_INST_PATH']}\n")
 
     cascadedir = designcfgs.get_design_cascade_path(fuzzerstate.design_name)
-    subprocess.run(cmd,cwd=cascadedir,env=env,capture_output=False)
+    try:
+        subprocess.run(cmd,cwd=cascadedir,env=env,capture_output=False)
+    except Exception as e:
+        os.removedirs(root_dir)
+        os.removedirs(trace_dir)
+        # os.removedirs(env_dir)
+        raise e
 
-    
-
-    # print(f"DrFUZZ: export COV_DIR={cov_dir_drfuzz}")
-    # print(f"RFUZZ: export COV_DIR={cov_dir_rfuzz}")
-    # print(f"FINAL: export SIMSRAMELF={rtl_elfpath}")
-    # print(f"INTERMEDIATE: export SIMSRAMELF={interm_elfpath}")
-    # print(f"export SIMSRAMTAINT={fuzzerstate.simsramtaint_path}")
-    # print(f"export EXPECTED_REVAlS={fuzzerstate.expected_regvals_path}")
-    # print(f"export MUT_INST_PATH={mut_inst_path}")
