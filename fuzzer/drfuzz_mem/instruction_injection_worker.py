17cc721b5686784944c68891e0d7fb4754d881ba
Tobias Kovats <tkovats@student.ethz.ch>
2024-03-22 12:06:05 +0000

    refactors for instruction_injection_worker.py

--- fuzzer/drfuzz_mem/instruction_injection_worker.py
+++ fuzzer/drfuzz_mem/instruction_injection_worker.py
@@ -5,6 +5,7 @@ import threading
 from common.spike import calibrate_spikespeed
 from common.profiledesign import profile_get_medeleg_mask
 from cascade.randomize.pickbytecodetaints import MAX_N_INJECT_PER_BB
+from cascade.util import CFInstructionClass
 callback_lock = threading.Lock()
 newly_finished_tests = 0
 total_finished_tests = 0
@@ -24,12 +25,11 @@ def test_done_callback(ret):
 
 
 
-def __gen_elf_and_inject_instructions_worker(design_name, max_n_insts_per_bb, en_taint, seed):
+def __gen_elf_and_inject_instructions_worker(design_name, max_n_insts_per_bb, en_taint, seed, fuzz_only_this_inst_type = None):
     try:
-        gen_elf_and_inject_instructions(design_name, max_n_insts_per_bb, en_taint, seed)
-        return 1
+        return gen_elf_and_inject_instructions(design_name, max_n_insts_per_bb, en_taint, seed, fuzz_only_this_inst_type)
     except Exception as e:
-        print(f"gen_elf_and_inct_taints_woker failed for: {design_name},{max_n_insts_per_bb},{seed}: {e}")
+        print(f"gen_elf_and_inject_taints_woker failed for: {design_name},{max_n_insts_per_bb},{seed}: {e}")
         return 0
 
 def inject_instructions(design_name: str, num_cores: int, total_tests: int, en_taint: str, seed_offset: int, can_authorize_priviles: bool):
@@ -37,6 +37,8 @@ def inject_instructions(design_name: str, num_cores: int, total_tests: int, en_t
     global callback_lock
 
     process_instance_id = 0
+    if seed_offset is not None:
+        process_instance_id += seed_offset
     num_workers = num_cores
     assert num_workers > 0
     calibrate_spikespeed()
@@ -44,7 +46,11 @@ def inject_instructions(design_name: str, num_cores: int, total_tests: int, en_t
     if num_workers == 1:
         for _ in range(total_tests):
             print(f"Starting instruction injection on `{design_name}`.")
+            # try:
             gen_elf_and_inject_instructions(design_name,MAX_N_INJECT_PER_BB,en_taint, process_instance_id)
+            # except Exception as e:
+            #     print(e)
+            process_instance_id += 1
         exit(0)
 
     print(f"Starting parallel instruction injection on {total_tests} total tests of `{design_name}` on {num_workers} processes.")
