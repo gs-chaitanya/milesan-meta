3ad42b306ef0bd6ecba6dda7f83945c2ddb0a027
Tobias Kovats <tkovats@student.ethz.ch>
2024-04-05 15:20:35 +0000

    Replaces taintloader with original cellift version.

--- design-processing/common/dv/taintloader.cc
+++ design-processing/common/dv/taintloader.cc
@@ -7,7 +7,6 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <tuple>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -23,28 +22,18 @@
 // First  dim: absolute word address.
 // Second dim: content.
 std::vector<std::unordered_map<size_t, std::vector<uint8_t>>> word_taint_contents;
-std::vector<std::unordered_map<size_t, std::pair<std::vector<uint8_t>,std::vector<uint8_t>> >> word_taint_data_contents;
 static size_t glob_word_width_bytes = -1;
 
 extern "C" {
   void init_taint_vectors(long long num_taints);
-  void init_taint_data_vectors(long long num_taints);
   char read_taint_section(long long taint_id, long int *word_address, const svOpenArrayHandle svbuffer);
   void read_taints(const char* filename, int word_width_bytes);
-  void read_taints_and_data(const char* filename, int word_width_bytes);
   char get_next_taint_word(long long taint_id, long int *word_address, const svOpenArrayHandle svbuffer);
-  char get_next_taint_and_data_word(long long taint_id, long int *word_address, const svOpenArrayHandle sv_tbuffer,  const svOpenArrayHandle sv_dbuffer);
 }
 
 extern "C" void init_taint_vectors(long long num_taints) {
   word_taint_contents.resize(num_taints);
 }
-
-extern "C" void init_taint_data_vectors(long long num_taints) {
-  word_taint_data_contents.resize(num_taints);
-  // for(auto &taint_data_pair: word_taint_data_contents) taint_data_pair.clear();
-}
-
 
 static inline char char_to_hex(char c) {
   if (c >= '0' && c <= '9')
@@ -71,6 +60,7 @@ extern "C" char get_next_taint_word(long long taint_id, long int *word_address, 
 
   // Supply the word address
   *word_address = itr->first;
+  printf("Next taint word addr: %lx\n", *word_address);
 
   // Fill the svbuffer with the word contents
   unsigned char* buf = (unsigned char*)svGetArrayPtr(svbuffer);
@@ -142,98 +132,3 @@ extern "C" void read_taints(const char* filename, int word_width_bytes) {
 
   f.close();
 }
-
-// @return 1 if this is a valid response, 0 if there is nothing anymore to supply, and the outputs should be ignored.
-extern "C" char get_next_taint_and_data_word(long long taint_id, long int *word_address, const svOpenArrayHandle sv_tbuffer,  const svOpenArrayHandle sv_dbuffer) {
-  static auto itr = word_taint_data_contents[taint_id].begin();
-
-  // If the end of the structure was reached.
-  if (itr == word_taint_data_contents[taint_id].end()) {
-    // Set it back to the beginning so another mem can read it as well (used in Ibex).
-    itr = word_taint_data_contents[taint_id].begin();
-    return 0;
-  }
-
-  // Supply the word address
-  *word_address = itr->first;
-
-  // Fill the svbuffer with the word contents
-  unsigned char* tbuf = (unsigned char*)svGetArrayPtr(sv_tbuffer);
-  unsigned char* dbuf = (unsigned char*)svGetArrayPtr(sv_dbuffer);
-  for (size_t byte_id_in_word = 0; byte_id_in_word < glob_word_width_bytes; byte_id_in_word++){
-    // printf("  Byte %d: %lx\n", byte_id_in_word, itr->second[byte_id_in_word]);
-    tbuf[byte_id_in_word] = itr->second.first[byte_id_in_word];
-    dbuf[byte_id_in_word] = itr->second.second[byte_id_in_word];
-  }
-
-  // Increment the iterator and return that the result is valid.
-  itr++;
-  return 1;
-}
-
-
-extern "C" void read_taints_and_data(const char* filename, int word_width_bytes) {
-  assert (word_width_bytes > 0);
-  glob_word_width_bytes = word_width_bytes;
-
-  std::ifstream f;
-  f.open(filename);
-
-  uint64_t taint_id;
-  uint64_t base_addr;
-  std::string base_addr_str;
-  uint64_t num_bytes;
-  std::string num_bytes_str;
-  std::string byte_taint_contents_str;
-  std::string byte_data_contents_str;
-
-  std::string tmp_line;
-  while (std::getline(f, tmp_line)) {
-    if (!tmp_line.size())
-        continue;
-    std::istringstream isstream(tmp_line);
-    isstream >> taint_id;
-    isstream >> base_addr_str;
-    isstream >> num_bytes_str;
-    isstream >> byte_taint_contents_str;
-    isstream >> byte_data_contents_str;
-
-    std::istringstream str_to_hex_stream_base_addr(base_addr_str);
-    str_to_hex_stream_base_addr >> std::hex >> base_addr;
-
-    std::istringstream str_to_hex_stream_num_bytes(num_bytes_str);
-    str_to_hex_stream_num_bytes >> std::hex >> num_bytes;
-
-    assert(num_bytes > 0);
-
-    if (byte_taint_contents_str.size() < 2*num_bytes) {
-      std::cerr << "Error: Bytes mask is not long enough in line: `" << tmp_line << "`." << std::endl;
-      exit(1);
-    }
-
-    // WARNING curr_word_id has an offset: curr_word_id does NOT correspond to address 0!
-    for (size_t curr_byte_addr = base_addr; curr_byte_addr < base_addr+num_bytes; curr_byte_addr ++) {
-      size_t curr_abs_word_addr = curr_byte_addr / word_width_bytes;
-      // Compute the curr byte content
-      size_t byte_offset = curr_byte_addr - base_addr;
-      uint8_t tbyte_val = (char_to_hex(byte_taint_contents_str[2*byte_offset]) << 4) | char_to_hex(byte_taint_contents_str[2*byte_offset+1]); 
-      uint8_t dbyte_val = (char_to_hex(byte_data_contents_str[2*byte_offset]) << 4) | char_to_hex(byte_data_contents_str[2*byte_offset+1]); 
-
-      // If the word is not yet in the new words struct, then add it.
-      if (word_taint_data_contents[taint_id].find(curr_abs_word_addr) == word_taint_data_contents[taint_id].end()) {
-        std::vector<uint8_t> new_taint_content(word_width_bytes, 0);
-        std::vector<uint8_t> new_data_content(word_width_bytes, 0);
-        std::pair<std::vector<uint8_t>,std::vector<uint8_t>> taint_data_pair(new_taint_content, new_data_content);
-        word_taint_data_contents[taint_id].insert({curr_abs_word_addr, taint_data_pair});
-      }
-
-      // Add the content to the word
-      size_t byte_id_in_word = curr_byte_addr % word_width_bytes;
-      word_taint_data_contents[taint_id][curr_abs_word_addr].first[byte_id_in_word] = tbyte_val;
-      word_taint_data_contents[taint_id][curr_abs_word_addr].second[byte_id_in_word] = dbyte_val;
-    }
-  }
-
-  f.close();
-}
-
