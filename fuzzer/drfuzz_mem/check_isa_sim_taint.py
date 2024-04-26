1ec99618651f9575a1abe644c558f62740655d1c
Tobias Kovats <tkovats@student.ethz.ch>
2024-04-26 07:52:05 +0000

    Some refactors.

--- fuzzer/drfuzz_mem/check_isa_sim_taint.py
+++ fuzzer/drfuzz_mem/check_isa_sim_taint.py
@@ -3,7 +3,7 @@ import shutil
 import glob
 import json
 
-from params.runparams import PATH_TO_TMP, PATH_TO_COV, PRINT_INSTRUCTION_EXECUTION_FINAL, PRINT_ENVIRONMENT, INSERT_REGDUMPS, PRINT_REGISTER_VALIDATION, PRINT_MEMORY_VALIDATION, PRINT_SKIPPED_CHECKS
+from params.runparams import PATH_TO_TMP, PATH_TO_COV, PRINT_INSTRUCTION_EXECUTION_FINAL, PRINT_ENVIRONMENT, INSERT_REGDUMPS, PRINT_REGISTER_VALIDATION, PRINT_MEMORY_VALIDATION, PRINT_SKIPPED_CHECKS, PRINT_AND_COMPARE
 from params.fuzzparams import USE_SPIKE_INTERM_ELF
 from cascade.fuzzfromdescriptor import NUM_MAX_BBS_UPPERBOUND, gen_fuzzerstate_elf_expectedvals_interm, gen_fuzzerstate_elf_expectedvals, gen_new_test_instance
 from cascade.cfinstructionclasses import *
@@ -26,8 +26,6 @@ def check_isa_sim_taint(design_name: str,seed: int):
 
     expected_intregvals = expected_regvals[0]
 
-    # Expected regvals of the program where bit was flipped are in in pc_reg_pairs1, which is the one that will be executed in the crossvalidation.
-    # Initial program register dumps are in pc_reg_pairs_0, TODO: should this also be executed and checked?
     env = fuzzerstate.setup_env(interm_elfpath if USE_SPIKE_INTERM_ELF else rtl_elfpath,seed)
 
     regstream_rtl, final_regvals_rtl, final_sramdump_rtl = run_rtl_and_load_regstream(env, fuzzerstate.design_name)
@@ -39,26 +37,28 @@ def check_isa_sim_taint(design_name: str,seed: int):
     fuzzerstate.csrfile.reset()
     # print(f"Starting spike cross-validation. (FSM instructions are accounted for.)")
     regdump_idx = 0
-    for bb_instrs in fuzzerstate.instr_objs_seq: # skip first and last bb
-        for next_instr in bb_instrs:
-            if isinstance(next_instr, RegdumpInstruction_t0) and INSERT_REGDUMPS:
-                next_instr.check_regs(regstream_rtl_val[regdump_idx]) # check value before executing instruction
-                next_instr.check_regs_t0(regstream_rtl_val_t0[regdump_idx]) # check value before executing instruction
-                regdump_idx += 1
-            elif not is_placeholder(next_instr):
-                next_instr.check_regs(pc_reg_pairs[next_instr.addr]) # check value before executing instruction. Skip if placeholder as their values change between spikeresol and final elf.
-            elif PRINT_SKIPPED_CHECKS:
-                print(f"Skipping check for {next_instr.get_str(USE_SPIKE_INTERM_ELF)}")
-
-            next_instr.execute(fuzzerstate.taint_en, is_spike_resolution=USE_SPIKE_INTERM_ELF)
-            if PRINT_INSTRUCTION_EXECUTION_FINAL:
-                next_instr.print(USE_SPIKE_INTERM_ELF)
-
     try:
+        for bb_instrs in fuzzerstate.instr_objs_seq: # skip first and last bb
+            for next_instr in bb_instrs:
+                if isinstance(next_instr, RegdumpInstruction_t0) and INSERT_REGDUMPS:
+                    if not USE_SPIKE_INTERM_ELF:
+                        next_instr.check_regs(regstream_rtl_val[regdump_idx]) # check value before executing instruction
+                        if TAINT_EN:
+                            next_instr.check_regs_t0(regstream_rtl_val_t0[regdump_idx]) # check value before executing instruction
+                        regdump_idx += 1
+                elif not is_placeholder(next_instr) and next_instr.addr in pc_reg_pairs:
+                    next_instr.check_regs(pc_reg_pairs[next_instr.addr]) # check value before executing instruction. Skip if placeholder as their values change between spikeresol and final elf.
+                elif PRINT_SKIPPED_CHECKS:
+                    print(f"Skipping check for {next_instr.get_str(USE_SPIKE_INTERM_ELF)}")
+
+                next_instr.execute(fuzzerstate.taint_en, is_spike_resolution=USE_SPIKE_INTERM_ELF)
+                if PRINT_INSTRUCTION_EXECUTION_FINAL:
+                    next_instr.print(USE_SPIKE_INTERM_ELF)
+
         for (addr_in_situ,trace_in_situ),(addr_final, trace_final) in zip(fuzzerstate.intregpickstate.writeback_trace_in_situ.items(),fuzzerstate.intregpickstate.writeback_trace_final.items()):
             assert addr_in_situ == addr_final
             assert trace_in_situ[0] == trace_final[0]
-            assert trace_in_situ[1] == trace_final[1], f"Mismatch in taint trace between in-situ simulation and final elf: {hex(addr_final)}: {ABI_INAMES[trace_in_situ[0]]} <- {hex(trace_in_situ[1])}/{hex(trace_final[1])} (in-situ/final).{filter_reg_t0_traceback(trace_in_situ[0],addr_in_situ,fuzzerstate).get_str(False)}"
+            assert trace_in_situ[1] == trace_final[1], f"Mismatch in taint trace between in-situ simulation and final elf: {hex(addr_final)}: {ABI_INAMES[trace_in_situ[0]]} <- {hex(trace_in_situ[1])}/{hex(trace_final[1])} (in-situ/final).{filter_reg_t0_traceback(trace_in_situ[0],addr_in_situ,fuzzerstate).get_str()}"
             # else:
             #     print(f"{hex(addr_spike)}: {ABI_INAMES[trace_spike[0]]} <- {hex(trace_spike[1])}")
 
@@ -69,33 +69,30 @@ def check_isa_sim_taint(design_name: str,seed: int):
             value = int(final_regvals_rtl[id]["value"],16)
             value_t0 = int(final_regvals_rtl[id]["value_t0"],16)
             mismatch = fuzzerstate.intregpickstate.regs[id+1].check(value)
-            assert not mismatch, f"Value mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str(False)}"
+            assert not mismatch, f"Value mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str()}"
             mismatch = fuzzerstate.intregpickstate.regs[id+1].check(expected_intregvals[id])
-            assert not mismatch, f"Value mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str(False)}"
-            mismatch = fuzzerstate.intregpickstate.regs[id+1].check_t0(value_t0)
-            assert not mismatch, f"Taint mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str(False)}"
+            assert not mismatch, f"Value mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str()}"
+            # mismatch = fuzzerstate.intregpickstate.regs[id+1].check_t0(value_t0)
+            # assert not mismatch, f"Taint mismatch for {mismatch[0]}: {hex(mismatch[1])} != {hex(mismatch[2])}\n\t Traceback: {filter_reg_traceback(id+1, None, fuzzerstate, None, False).get_str()}"
 
         if PRINT_MEMORY_VALIDATION:
             print("*** MEMORY VALIDATION ***:")
             fuzzerstate.memview.print_and_compare(final_sramdump_rtl)
         if fuzzerstate.design_name == "kronos":
             fuzzerstate.memview.check(final_sramdump_rtl)
-
-        # print("Ok.")
+        # # print("Ok.")
 
     except Exception as e:
-        # os.removedirs(trace_dir)
-        # if os.path.isfile(env_path): os.remove(env_path)
-        # if os.path.isfile(interm_elfpath): os.remove(interm_elfpath)
-        print("*** REGISTER VALIDATION FAILED ***")
-        fuzzerstate.intregpickstate.print_and_compare(final_regvals_rtl)
-        if fuzzerstate.design_name == "kronos":
-            print("*** MEMORY CONTENT  ***")
-            fuzzerstate.memview.print_and_compare(final_sramdump_rtl)
+        if PRINT_AND_COMPARE:
+            print("*** REGISTER VALIDATION FAILED ***")
+            fuzzerstate.intregpickstate.print_and_compare(final_regvals_rtl)
+            if fuzzerstate.design_name == "kronos": # kronos does not have a cache so we can validate the memory
+                print("*** MEMORY CONTENT  ***")
+                fuzzerstate.memview.print_and_compare(final_sramdump_rtl)
         print(f"Failed for seed {seed}")
-        raise e
+        raise type(e)(f"{fuzzerstate.instance_to_str()}: {e}")
 
-    return True
+    return fuzzerstate
 
 
 
--- fuzzer/drfuzz_mem/check_isa_sim_worker.py
+++ fuzzer/drfuzz_mem/check_isa_sim_worker.py
@@ -1,13 +1,17 @@
 from drfuzz_mem.check_isa_sim import check_isa_sim
 from drfuzz_mem.check_isa_sim_taint import check_isa_sim_taint
-import multiprocessing as mp
-import time
-import threading
 from common.spike import calibrate_spikespeed
 from common.profiledesign import profile_get_medeleg_mask
 from cascade.randomize.pickbytecodetaints import MAX_N_INJECT_PER_BB
 from cascade.util import CFInstructionClass
+from params.runparams import PATH_TO_TMP
 
+import multiprocessing as mp
+import time
+import threading
+import os
+
+LOG_EXCEPTIONS = True
 PRINT_THREAD_STATUS = False
 callback_lock = threading.Lock()
 newly_finished_tests = 0
@@ -23,7 +27,7 @@ def test_done_callback(ret):
         if(ret):
             total_finished_tests += 1
             if PRINT_THREAD_STATUS:
-                print(f"Finished {total_finished_tests} threads.")
+                print(f"Finished {total_finished_tests} succesful threads.")
         # else:
         #     print(f"Thread failed.")
 
@@ -32,12 +36,19 @@ def test_done_callback(ret):
 def __check_isa_sim_worker(design_name, seed, taint_en):
     try:
         if taint_en:
-            return check_isa_sim_taint(design_name,seed)
+            check_isa_sim_taint(design_name,seed).remove_tmp_files()
         else:
-            return check_isa_sim(design_name, seed)
+            check_isa_sim(design_name,seed)
+        return True
     except Exception as e:
         print(f"check_isa_sim_worker failed for {design_name} with seed {seed}: {e}")
-        return 0
+        if LOG_EXCEPTIONS:
+            if "There are less" not in str(e):
+                logdir = os.path.join(PATH_TO_TMP, "logs")
+                os.makedirs(logdir, exist_ok=True)
+                with open(f"{logdir}/{design_name}.log", "a") as f:
+                    f.write(f"{str(e)}\n")
+        return False
 
 def check_isa_sims(design_name: str, num_cores: int, total_tests: int, taint_en: bool, seed_offset: int):
     global newly_finished_tests
