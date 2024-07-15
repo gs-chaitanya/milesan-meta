41e6257d29fcd1bfe2eacbc1fa14ba4bb841b3c5
Tobias Kovats <tkovats@student.ethz.ch>
2024-07-15 15:29:22 +0000

    Disables INSERT_REGDUMPS and INSERT_FENCE.

--- fuzzer/params/runparams.py
+++ fuzzer/params/runparams.py
@@ -18,7 +18,7 @@ DO_EXPENSIVE_ASSERT = False # More expensive assertions
 
 NO_REMOVE_TMPFILES = False # Used for debugging purposes
 # TODO: below currently need to be enabled for reduction with pillar.
-NO_REMOVE_TMPDIRS = True # When disabled, removes the /cascade-data/[design-name]/[ID] directories even when leakage (or bug) detected. Enable to save storage when fuzzing multi-threaded.
+NO_REMOVE_TMPDIRS = False # When disabled, removes the /cascade-data/[design-name]/[ID] directories even when leakage (or bug) detected. Enable to save storage when fuzzing multi-threaded.
 
 RUN_TIMEOUT_SECONDS = 60*3 # A program is not supposed to run longer than this in RTL simulation.
 
@@ -36,7 +36,7 @@ PRINT_WRITEBACK = False
 PRINT_INSTRUCTION_EXECUTION_IN_SITU = False # Prints execution during program generation.
 PRINT_INSTRUCTION_EXECUTION_FINAL = False # Prints execution during register value checks.
 PRINT_INSTRUCTION_EXECUTION_REDUCE = False # Prints execution during program reduction.
-PRINT_INSTRUCTION_EXECUTION_REGDUMP_REQS = True
+PRINT_INSTRUCTION_EXECUTION_REGDUMP_REQS = False
 PRINT_COLOR_TAINT = True
 
 PRINT_REG_TRACEBACK = False
@@ -44,8 +44,8 @@ PRINT_FILTERED_REG_TRACEBACK = False
 
 PRINT_ENVIRONMENT = False
 
-INSERT_REGDUMPS = True # Speculative bugs will likely diappear when enabled. Used to test correctness of dataflow computation.
-INSERT_FENCE = True # The stores should become architectually visible in order, so this should not be necessary in most cases with WT caches. CVA6 needs it.
+INSERT_REGDUMPS = False # Speculative bugs will likely diappear when enabled. Used to test correctness of dataflow computation.
+INSERT_FENCE = False # The stores should become architectually visible in order, so this should not be necessary in most cases with WT caches. CVA6 needs it.
 assert not (USE_MMU and INSERT_REGDUMPS), "Regdumps are not supported when MMU is enabled." # We would have to translate the regdump address for each context switch, otherwise not difficult to implement.
 assert not (INSERT_SPECTRE_GADGETS and INSERT_REGDUMPS), "Regdumps are not supported when spectre gadgets enabled."
 assert not (TAINT_NONTAKEN_BRANCH_IMM and INSERT_REGDUMPS), "Enabling TAINT_NONTAKEN_BRANCH_IMM might render INSERT_REGDUMPS useless as pc might get tainted if non-taken branch is predicted taken."
