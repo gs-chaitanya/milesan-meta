7aea947ddcb1ed2829e62f8240e11102844f43a1
Tobias Kovats <tkovats@student.ethz.ch>
2024-07-24 06:13:17 +0000

    Assert design supports compressed when USE_COMPRESSED is enabled.

--- fuzzer/common/designcfgs.py
+++ fuzzer/common/designcfgs.py
@@ -5,7 +5,7 @@
 import json
 import os
 from functools import cache
-
+from params.fuzzparams import USE_COMPRESSED
 DESIGN_REPOS_JSON_NAME = "design_repos.json"
 
 def is_design_name_known(design_name: str):
@@ -67,7 +67,9 @@ def get_design_march_ccflags_nocompressed(design_name) -> int:
 # @param design_name: must be one of the keys of the design_repos.json dict.
 # @return for example `rv64gc`.
 def get_design_march_flags(design_name) -> str:
-    return get_design_march_ccflags(design_name).split('-march=')[1].split(' ')[0].lower()
+    flags = get_design_march_ccflags(design_name).split('-march=')[1].split(' ')[0].lower()
+    assert not (USE_COMPRESSED and 'c' not in flags)
+    return flags
 
 def get_design_march_flags_nocompressed(design_name) -> str:
     return get_design_march_ccflags(design_name).split('-march=')[1].split(' ')[0].lower().replace('c', '')
