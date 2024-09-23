5992a6fd703b5bd0dd09072201f44473e24e099e
Tobias Kovats <tkovats@student.ethz.ch>
2024-09-23 08:41:19 +0000

    Suppresses some warnings in modelsim, adds openc910.

--- design-processing/common/modelsim/modelsim_run.tcl
+++ design-processing/common/modelsim/modelsim_run.tcl
@@ -27,7 +27,7 @@ if { [string equal $TRACE trace_fst] } {
     set DEBUGDBARG ""
 }
 
-vsim -64 -lib $LIB $MODELSIM_VSIM_COVERFLAG  $DEBUGDBARG $VOPTARGS tb_top
+vsim -64  -suppress 3009 -lib $LIB $MODELSIM_VSIM_COVERFLAG  $DEBUGDBARG $VOPTARGS tb_top
 
 if { [string equal $TRACE trace_fst] } { 
     log -r /*
