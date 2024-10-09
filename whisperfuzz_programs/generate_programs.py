e368413529b63c5e1fafc5196c6984d773325e62
Tobias Kovats <tkovats@student.ethz.ch>
2024-10-09 12:35:28 +0000

    Modifies whisperfuzz poc program generation and automatic testing on boom and cva6.

--- whisperfuzz_programs/generate_programs.py
+++ whisperfuzz_programs/generate_programs.py
@@ -11,11 +11,12 @@ from fuzzer.common.designcfgs import get_design_stop_sig_addr, get_design_reg_du
 #%%%
 DUTS = ["cva6-test"]
 SECTION_STR = ".section \".text.init\",\"ax\",@progbits\n\t.globl _start\n\t.align 2\n_start:\n"
-# INSTR_STRS = ["mv","add","and","or","xor","sub","addw","subw"]
-INSTR_STRS = ["divuw"]
-# CINSTR_STR = [f"c.{inst}" for inst in INSTR_STRS] 
+INSTR_STRS = ["mv","add","and","or","xor","sub","addw","subw"]
+# INSTR_STRS = ["divuw"]
+CINSTR_STR = [f"c.{inst}" for inst in INSTR_STRS] 
 CINSTR_STR= [] 
-IMMS = [0] + [1<<i for i in range(20)]
+# IMMS = [0] + [1<<i for i in range(20)]
+IMMS = [0,2047]
 CWD =  "/mnt/cascade-meta/whisperfuzz_programs/"
 SRCDIR = "/mnt/cascade-meta/whisperfuzz_programs/src"
 BUILDDIR = "/mnt/cascade-meta/whisperfuzz_programs/build"
@@ -23,10 +24,12 @@ BUILDDIR = "/mnt/cascade-meta/whisperfuzz_programs/build"
 def generate_program(inst_str: str, imm: int, stopsig_addr: int, regdump_addr: int, clear_csr = bool):
     prog_str = SECTION_STR
     prog_str += f"li a4, {imm}\n"
+    prog_str += f"li a7, 0\n"
     if clear_csr:
-        prog_str += "csrwi mcycle, 0\n"
-    prog_str += f"{inst_str} a3, a4\n" if "mv" in inst_str or "c" in inst_str else  f"{inst_str} a3, a3, a4\n"
+        prog_str += "csrr t1, mcycle\n"
+    prog_str += f"{inst_str} t3, a4\n" if "mv" in inst_str or "c" in inst_str else  f"{inst_str} t3, t3, a4\n"
     prog_str += "csrr ra, mcycle\n"
+    prog_str += f"sub ra, ra, t1\n"
     prog_str += f"li a0, {regdump_addr}\n"
     prog_str += "sd ra, 0(a0)\n"
     prog_str += f"li a0, {stopsig_addr}\n"
@@ -41,12 +44,12 @@ clear_csrs = []
 for dut in DUTS:
     stopsig_addr = get_design_stop_sig_addr(dut)
     regdump_addr = get_design_reg_dump_addr(dut)
-    for instr_str in INSTR_STRS+CINSTR_STR:
+    for instr_str in ["c.add"]:
         for imm in IMMS:
-            for clear_csr in [True, False]:
+            for clear_csr in [True]:
                 prog = generate_program(instr_str, imm, stopsig_addr, regdump_addr,clear_csr)
-                with open(f"{SRCDIR}/{instr_str}_{imm}_{int(clear_csr)}.S", "w") as f:
-                    f.write(prog)
+                # with open(f"{SRCDIR}/{instr_str}_{imm}_{int(clear_csr)}.S", "w") as f:
+                    # f.write(prog)
                 targets += [f"build/{instr_str}_{imm}_{int(clear_csr)}.elf"]
                 instr_strs += [instr_str]
                 imms += [imm]
@@ -60,7 +63,7 @@ subprocess.run(["make","all"],cwd=CWD,env=env)
 # %%
 env = os.environ.copy()
 env["SIMLEN"] = "1000"
-# mcycle_df = pd.DataFrame()
+mcycle_df = pd.DataFrame()
 for dut in DUTS:
     for target,instr,imm,clear_csr in zip(targets,instr_strs,imms,clear_csrs):
         env["SIMSRAMELF"] = f"{CWD}/{target}"
