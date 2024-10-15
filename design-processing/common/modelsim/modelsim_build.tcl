d6654c7116cafb8cbca66202604fbe8da9831aae
Tobias Kovats <tkovats@student.ethz.ch>
2024-10-15 13:40:07 +0000

    Adds rocket-medium and rocket-large design repo paths, disables modelsim vopt if tracing is enabled.

--- design-processing/common/modelsim/modelsim_build.tcl
+++ design-processing/common/modelsim/modelsim_build.tcl
@@ -35,6 +35,9 @@ vlog -64 -ccflags '-std=c++14' -work $LIB $MODELSIM_VLOG_COVERFLAG -dpiheader $C
 
 vlog -64 -suppress 3009 -sv -work $LIB $MODELSIM_VLOG_COVERFLAG $CASCADE_META_COMMON/dv/sv/rst_gen.sv $CASCADE_META_COMMON/dv/sv/clk_rst_gen.sv $SV_TOP
 
-vopt -64 -sv -work $LIB tb_top -o tb_top_opt
+# Optimize only if we don't need tracing.
+if { $TRACE == "trace_fst" || $TRACE == "trace" } {
+    vopt -64 -sv -work $LIB tb_top -o tb_top_opt
+}
 
 quit -f
