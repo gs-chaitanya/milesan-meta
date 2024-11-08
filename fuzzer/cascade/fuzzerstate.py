fc460ad472ca8c77b3ea276f6659c89813e67e99
Tobias Kovats <tkovats@student.ethz.ch>
2024-11-08 18:18:56 +0000

    Bugfix taint_source_layouts.

--- fuzzer/cascade/fuzzerstate.py
+++ fuzzer/cascade/fuzzerstate.py
@@ -166,9 +166,9 @@ class FuzzerState:
     # @return [(MODE, #level_used)]
     def select_prog_mmu_params(self):
         self.num_layouts = random.randint(1, MAX_NUM_LAYOUTS)
-        num_taint_source_layouts = random.randint(MIN_N_TAINT_SOURCE_LAYOUTS,MAX_N_TAINT_SOURCE_LAYOUTS)
+        num_taint_source_layouts = random.randint(MIN_N_TAINT_SOURCE_LAYOUTS,MAX_N_TAINT_SOURCE_LAYOUTS) if MAX_N_TAINT_SOURCE_LAYOUTS!=-1 else self.num_layouts
         # -1 is always a taint_source_layout
-        self.taint_source_layouts = range(-1,num_taint_source_layouts if MAX_N_TAINT_SOURCE_LAYOUTS!=-1 else self.num_layouts)
+        self.taint_source_layouts = range(-1,num_taint_source_layouts)
         # self.taint_sink_layouts = range(num_taint_source_layouts,num_taint_source_layouts+num_taint_sink_layouts)
         if self.is_design_64bit:
             allowed_params = MODES_PARAMS_RV64
@@ -179,7 +179,7 @@ class FuzzerState:
             # n_level = random.randint(1, allowed_params[mode][2])
             n_level = allowed_params[mode][2]
             self.prog_mmu_params.append((mode, n_level))
-        if DEBUG_PRINT: print(f"generated parameters: {self.prog_mmu_params}: taint source layouts: {self.taint_source_layouts}")
+        if DEBUG_PRINT: print(f"generated parameters: {self.prog_mmu_params}: taint source layouts: {self.taint_source_layouts} ({num_taint_source_layouts}/{self.num_layouts})")
 
        
 
