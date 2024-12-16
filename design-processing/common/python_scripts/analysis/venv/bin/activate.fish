27325a2c1d48820857eeb7218fa134417776422c
Tobias Kovats <tkovats@student.ethz.ch>
2024-12-16 14:11:57 +0000

    Removes ignored design-processing/common/python_scripts/analysis/venv

--- design-processing/common/python_scripts/analysis/venv/bin/activate.fish
+++ design-processing/common/python_scripts/analysis/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/cascade-meta/design-processing/common/python_scripts/analysis/venv"
+set -gx VIRTUAL_ENV "/mnt/milesan-meta/design-processing/common/python_scripts/analysis/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
