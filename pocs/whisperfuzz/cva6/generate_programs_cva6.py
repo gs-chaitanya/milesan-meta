9f75a181782ad29d8c7a868bcc7379ab4d97c15c
Tobias Kovats <tkovats@student.ethz.ch>
2025-06-11 09:12:33 +0000

    Restructures whisperfuzz PoCs.

--- pocs/whisperfuzz/cva6/generate_programs_cva6.py
+++ pocs/whisperfuzz/cva6/generate_programs_cva6.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from fuzzer.common.designcfgs import get_design_stop_sig_addr, get_design_reg_dump_addr, get_design_milesan_path
 #%%%
-
+os.environ["MILESAN_DESIGN_PROCESSING_ROOT"] = "/mnt/milesan-meta/design-processing/"
 DUTS = ["cva6"]
 SECTION_STR = ".section \".text.init\",\"ax\",@progbits\n\t.globl _start\n\t.align 2\n_start:\n"
 INSTR_STRS = ["mv","add","and","or","xor","sub","addw","subw"]
@@ -57,38 +57,39 @@ for dut in DUTS:
                 targets += [f"build/{instr_str}.elf"]
                 instr_strs += [instr_str]
 
-# %%
-env = os.environ.copy()
-env["TARGETS"] = f"\"{' '.join(targets)}\""
-# subprocess.run(["make","clean"],cwd=CWD,env=env)
-subprocess.run(["make","all"],cwd=CWD,env=env)
+# # %%
+# env = os.environ.copy()
+# env["TARGETS"] = f"\"{' '.join(targets)}\""
+# # subprocess.run(["make","clean"],cwd=CWD,env=env)
+# subprocess.run(["make","all"],cwd=CWD,env=env)
 
-# %%
-env = os.environ.copy()
-env["SIMLEN"] = "1000"
-mcycle_df = pd.DataFrame()
-for dut in DUTS:
-    for target,instr,imm,clear_csr in zip(targets,instr_strs):
-        env["SIMSRAMELF"] = f"{CWD}/{target}"
-        cmd = ["make","run_vanilla_notrace"]
-        output = subprocess.run(cmd,cwd=get_design_milesan_path(dut),env=env, capture_output=True)
-        mcycles = int(re.findall("Dump of reg x01:\s*0x[0-9a-zA-Z]+",str(output))[0].split(":")[-1].strip(),16)
-        row = {
-            "target":target,
-            "instr": instr,
-            "imm":imm,
-            "mcycles":mcycles,
-            "iscompressed": "c" in instr,
-            "clear_csr":clear_csr
-        }
-        mcycle_df = pd.concat([mcycle_df,pd.DataFrame([row])])
+# # %%
+# env = os.environ.copy()
+# env["SIMLEN"] = "1000"
+# mcycle_df = pd.DataFrame()
+# for dut in DUTS:
+#     for target,instr,imm,clear_csr in zip(targets,instr_strs):
+#         env["SIMSRAMELF"] = f"{CWD}/{target}"
+#         cmd = ["make","run_vanilla_notrace"]
+#         output = subprocess.run(cmd,cwd=get_design_milesan_path(dut),env=env, capture_output=True)
+#         mcycles = int(re.findall("Dump of reg x01:\s*0x[0-9a-zA-Z]+",str(output))[0].split(":")[-1].strip(),16)
+#         row = {
+#             "target":target,
+#             "instr": instr,
+#             "imm":imm,
+#             "mcycles":mcycles,
+#             "iscompressed": "c" in instr,
+#             "clear_csr":clear_csr
+#         }
+#         mcycle_df = pd.concat([mcycle_df,pd.DataFrame([row])])
 
+# # %%
+# fig, ax = plt.subplots()
+# sns.scatterplot(data=mcycle_df[(mcycle_df["iscompressed"] == True) & (mcycle_df["clear_csr"] == 0) ], x="imm",y="mcycles",hue="instr",ax=ax)
+# ax.set_xscale("log",base=2)
+# # %%
+# fig, ax = plt.subplots()
+# sns.scatterplot(data=mcycle_df[(mcycle_df["iscompressed"] == False) & (mcycle_df["clear_csr"] == 0) & (mcycle_df["instr"] == "div")], x="imm",y="mcycles",hue="instr",ax=ax)
+# ax.set_xscale("log",base=2)
+# ax.set_ylim([270,280])
 # %%
-fig, ax = plt.subplots()
-sns.scatterplot(data=mcycle_df[(mcycle_df["iscompressed"] == True) & (mcycle_df["clear_csr"] == 0) ], x="imm",y="mcycles",hue="instr",ax=ax)
-ax.set_xscale("log",base=2)
-# %%
-fig, ax = plt.subplots()
-sns.scatterplot(data=mcycle_df[(mcycle_df["iscompressed"] == False) & (mcycle_df["clear_csr"] == 0) & (mcycle_df["instr"] == "div")], x="imm",y="mcycles",hue="instr",ax=ax)
-ax.set_xscale("log",base=2)
-ax.set_ylim([270,280])
