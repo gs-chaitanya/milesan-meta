ca0afd353b43317d0d3447a6577d0a3a0b8e6bfd
Tobias Kovats <tkovats@student.ethz.ch>
2025-09-01 13:46:28 +0000

    Remove reduction performance from eval.

--- fuzzer/benchmarking/run_all_benchmarks.sh
+++ fuzzer/benchmarking/run_all_benchmarks.sh
@@ -2,5 +2,4 @@
 python fuzz_and_reduce.py fuzzconfigs/trans-tte-boom.json # TTE plot of BOOM
 python fuzz_and_reduce.py fuzzconfigs/ct-violations-tte-openc910.json # TTE plot for OpenC910
 python fuzz_and_reduce.py fuzzconfigs/ct-violations-tte-cva6.json # TTE plot for CVA6
-python fuzz_and_reduce.py fuzzconfigs/throughput.json # fuzzing throuput plot
-python fuzz_and_reduce.py fuzzconfigs/reduction-perf.json # reduction component-wise performance
+python fuzz_and_reduce.py fuzzconfigs/throughput.json --fuzz-only # fuzzing throuput and performance split plots
